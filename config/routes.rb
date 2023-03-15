a4bb38d651e0ba78984d39279f2bb0e0b5c05ee0
Daniel Londoño <daniel.esloh@gmail.com>
2023-03-14 21:38:31 -0500

    finished project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :tasks
+  root 'tasks#index'
+  post 'tasks/:id/toggle', to: "tasks#toggle"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
